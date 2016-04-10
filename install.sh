97968288346b68531d3dd879470d269b111b4ea9
Usman Khan <usmankhan9190@gmail.com>
2016-04-10 14:28:56 -0400

    Completed install.sh. Remove Makefile.

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #
-# Install dotfiles in $HOME, followed by Vundle and vim plugins.
+# Install dotfiles in $HOME, followed by Vundle and vim plugins. Requires Git.
 #
 # NOTE: This script will clobber any existing <dotfile>.backup scripts in $HOME.
 #
 
+set -e
+
 # All the dotfiles that we care about. Note the lack of leading dot.
 dotfiles=(bashrc ghci gitconfig inputrc tmux.conf vimrc)
 
-# Home directory. TODO: Change to home_dir $HOME once testing is finished.
-home_dir=./temp
-
-# Vundle vars.
+home_dir=$HOME
 vundle_repo=http://github.com/VundleVim/Vundle.Vim
 vundle_dir=$home_dir/.vim/bundle/Vundle.vim
 
@@ -21,20 +20,23 @@ vundle_dir=$home_dir/.vim/bundle/Vundle.vim
 #   2. Install Vundle.
 #   3. Use Vundle to install vim plugins.
 install() {
+  # Create symlinks for dotfiles.
   for file in ${dotfiles[*]}
   do
     ln -svr --backup --suffix=.backup $file $home_dir/.$file
   done
 
-  echo "-- Install completed. ----------------------------"
-}
+  # Install Vundle.
+  if [[ ! -d $vundle_dir ]]; then
+    git clone $vundle_repo $vundle_dir
+  else
+    echo "Vundle already installed. Moving on..."
+  fi
 
-install_vundle() {
-  echo "Implement me!"
-}
+  # Install vim plugins using Vundle.
+  vim +PluginInstall +qall
 
-install_vim_plugins() {
-  echo "Implement me!"
+  echo "-- INSTALL COMPLETED -----------------------------"
 }
 
 install
